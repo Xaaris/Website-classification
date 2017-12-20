fbe33250838194fe1d9bf47ef4acd356269385e8
Hannes <johannes.berger1@haw-hamburg.de>
2017-12-20 18:57:19 +0100

    deleted links to pdfs

--- Clean_url_resource_file.py
+++ Clean_url_resource_file.py
@@ -16,7 +16,7 @@ lines_with_urls = set()
 path = "/Users/hannes/Downloads/content.rdf.u8"
 for line in read_lines_from_file(path):
     line_as_string = str(line)
-    if "<link r:resource=" in line_as_string and ".de/" in line_as_string:
+    if "<link r:resource=" in line_as_string and ".de/" in line_as_string and ".pdf" not in line_as_string:
         line_as_string = line_as_string.replace("b'    <link r:resource=\"", "").replace("\"></link>\\n'", "")
         lines_with_urls.add(line_as_string)
 
