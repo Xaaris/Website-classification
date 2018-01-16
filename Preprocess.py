ea79f64c4a0e457152d934c986f7eba984200800
Hannes <johannes.berger1@haw-hamburg.de>
2018-01-16 15:23:17 +0100

    preprocessing is now multithreaded

--- Preprocess.py
+++ Preprocess.py
@@ -1,4 +1,8 @@
 import glob
+import threading
+import time
+import unicodedata
+from multiprocessing import Process
 
 from langdetect import detect, lang_detect_exception
 from nltk.corpus import stopwords
@@ -6,11 +10,12 @@ from nltk.stem.snowball import GermanStemmer
 
 
 def remove_punctuation(word):
-    return ''.join(char for char in word if char not in ['.', '"', ',', '(', ')', '!', '?', ';', ':', 'ǀ', '+'])
+    punctuation = {'.', '"', ',', '(', ')', '!', '?', ';', ':', 'ǀ', '+', '|', '*', '[', ']', '{', '}', '\''}
+    return ''.join(char for char in word if char not in punctuation and unicodedata.category(char)[0] != "C")
 
 
 def is_short_word(word):
-    return len(word) > 2
+    return len(word) < 2
 
 
 def is_number(word):
@@ -22,7 +27,26 @@ def apply_stemming(word):
 
 
 def is_stopword(word):
-    return word in stopwords.words('german')
+    return word in cachedStopWords
+
+
+def lang_detect_and_preprocess(lines_of_text):
+    local_docs = []
+    for i, line in enumerate(lines_of_text):
+        if len(line) > 0:
+            try:
+                language = detect(line)
+                print(str(i) + " " + language)
+                if language == "de":
+                    processed_words = preprocess_doc(line.split())
+                    local_docs.append(" ".join(processed_words))
+            except lang_detect_exception.LangDetectException as error:
+                print(error)
+
+    threadLock.acquire()
+    for line in local_docs:
+        append_line_to_file(save_path, line)
+    threadLock.release()
 
 
 def preprocess_doc(words):
@@ -48,8 +72,12 @@ def append_line_to_file(filename, line):
 
 
 def read_lines_from_file(filename):
-    with open(filename) as file:
-        return file.readlines()
+    lines = []
+    with open(filename, "r") as file:
+        for line in file:
+            line = bytes(line, 'utf-8').decode('utf-8', 'ignore')
+            lines.append(line)
+    return lines
 
 
 def get_file_names_from_directory(path, ending):
@@ -62,31 +90,43 @@ def read_file_from_disc(path):
     return file.read()
 
 
-docs_de = []
-load_path = "./data/negative/visible_text/successful_text_extractions.txt"
-save_path = "./data/negative/visible_text/preprocessed_text.txt"
-stemmer = GermanStemmer()
-for i, line in enumerate(read_lines_from_file(load_path)):
-    if len(line) > 0:
-        try:
-            language = detect(line)
-            print(str(i) + " " + language + ": " + line)
-            if language == "de":
-                processed_words = preprocess_doc(line.split())
-                docs_de.append(processed_words)
-        except lang_detect_exception.LangDetectException as error:
-            print(error)
+def chunk(url_list, number_of_chunks):
+    length_of_list = len(url_list)
+    assert 0 < number_of_chunks <= length_of_list
+    chunk_size = (length_of_list // number_of_chunks) + 1
+    return [url_list[p:p + chunk_size] for p in range(0, length_of_list, chunk_size)]
 
-write_docs_to_disc(save_path, docs_de)
+
+# caching
+detect("this is just for loading")
+cachedStopWords = stopwords.words('german')
+
+threadLock = threading.Lock()
+stemmer = GermanStemmer()
+
+load_path = "./data/positive/visible_text/successful_text_extractions.txt"
+save_path = "./data/positive/visible_text/preprocessed_text.txt"
+
+
+# start = time.time()
+# print("STARTED!")
+#
+# number_of_processes = 8
+#
+# lines = read_lines_from_file(load_path)
+# chunks = chunk(lines, number_of_processes)
+#
+# processes = []
+# if __name__ == '__main__':
+#     for i in range(number_of_processes):
+#         process = Process(target=lang_detect_and_preprocess, args=(chunks[i],))
+#         process.start()
+#         processes.append(process)
+#     for t in processes:
+#         t.join()
+#
+# end = time.time()
+# print("DONE")
+# print("Time: " + str(end - start))
 
 # nltk.download('stopwords')
-# url_list = read_lines_from_file("urls.txt")
-#
-# docs = []
-# for url in url_list:
-#     html = download_html_from_url(url)
-#     processed_words = preprocess_doc(html)
-#     print(str(len(processed_words)) + " " + str(processed_words))
-#     docs.append(processed_words)
-#
-# write_docs_to_disc("docs.txt", docs)
